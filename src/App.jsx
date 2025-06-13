28468cddb0daeff71cc3fcadff4333bf7d86c262
WallyDev <wallace07abreu@gmail.com>
2025-06-13 14:50:42 -0300

    Change EndPoints Web

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   const getUsers = async () => {
     try {
-      const res = await axios.get("http://localhost:8080/api/person");
+      const res = await axios.get("https://back-is-v0x1.onrender.com/api/person");
       setList(res.data);
       console.log(res.data); 
     } catch (error) {
@@ -21,7 +21,7 @@ function App() {
 
   const registerUser = async () => {
     try { 
-      await axios.post("http://localhost:8080/api/person",{
+      await axios.post("https://back-is-v0x1.onrender.com/api/person",{
         name, lastname, salary
       });
     } catch (error) {
@@ -31,7 +31,7 @@ function App() {
 
   const deleteUser = async (id) => {
     try { 
-      await axios.delete(`http://localhost:8080/api/person/${id}`);
+      await axios.delete(`https://back-is-v0x1.onrender.com/api/person/${id}`);
       getUsers()
     } catch (error) {
       console.error("Erro ao inserir", error);
